3fbe7c2e1152e3ee268643dbee560bf43fefc84d
sanjaikumar263 <sanjaikumar260301@gmail.com>
2026-02-10 23:53:01 +0530

    Cors Allow

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ dotenv.config();
 const app = express();
 
 
-// Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 app.use(express.json());
 
 
